db1fc0ba3424badb4b5e24094f18539ca5da3ae2
Штукина Катя <148587785+shtukina@users.noreply.github.com>
2024-10-20 20:23:11 +0300

    Update number1.py

--- лабораторная1/number1.py
+++ лабораторная1/number1.py
@@ -1,8 +1,8 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-sum = sum(numbers[:4] + numbers[5:])
-lenght = len(numbers)
-end = sum/lenght
-numbers[4] = end
+a = sum(numbers[:4] + numbers[5:])
+b = len(numbers)
+result = a/b
+numbers[4] = result
 # TODO заменить значение пропущенного элемента средним арифметическим
 
 print("Измененный список:", numbers)
