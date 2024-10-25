fd57eb71ec858c3e9b2d2db1526e847e0c6c537b
Штукина Катя <148587785+shtukina@users.noreply.github.com>
2024-10-25 16:38:23 +0300

    Update number1.py

--- лабораторная1/number1.py
+++ лабораторная1/number1.py
@@ -1,7 +1,7 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-a = sum(numbers[:4] + numbers[5:])
-b = len(numbers)
-result = a/b
+sum_of_numbers = sum(numbers[:4] + numbers[5:])
+length = len(numbers)
+result = sum_of_numbers / length
 numbers[4] = result
 # TODO заменить значение пропущенного элемента средним арифметическим
 
